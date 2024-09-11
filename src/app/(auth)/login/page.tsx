42a2e59cd0beecfd6f354bec76440c6775b27262
MRoyhanF <froyhan0@gmail.com>
2024-09-11 23:16:11 +0700

    chpt:4 Link and navigation

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+
+export default function LoginPage() {
+    return (
+    <div className="h-screen w-100 flex justify-center items-center">
+        <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <div className="flex items-center justify-center p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+                    Sign in 
+                </h3>
+            </div>
+            <div className="p-4 md:p-5">
+                <form className="space-y-4" action="#">
+                    <div>
+                        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                        <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" required />
+                    </div>
+                    <div>
+                        <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
+                        <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
+                    </div>
+                    <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login</button>
+                    <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
+                        Not registered? 
+                        <Link href="/register" className="text-blue-700 hover:underline dark:text-blue-500">Create account</Link>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    );
+}
